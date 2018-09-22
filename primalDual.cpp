3f7f67705f146c81ee9b3b59176703b15ab2cbd0
Renata <renatassmendes@hotmail.com>
2018-09-22 19:45:02 -0300

    ja varias coisas definidas

--- primalDual.cpp
+++ primalDual.cpp
@@ -19,14 +19,19 @@ CLASSE DE CLIENTES
 class D
 {
 	int id;
+	float v; // quantia que o cliente vai contribuir
 public:
 	D(int ID);
 	int get_id();
 	void set_id(int id);
+	float get_v();
+	void set_v(int v);
+	void aumentar_v(int qtd);
 };
 
 D::D(int id){
 	this->id = id;
+	this->v = 0; // inicialmente nao contribui com nada
 }
 
 int D::get_id(){
@@ -36,6 +41,19 @@ int D::get_id(){
 void D::set_id(int id){
 	this->id = id;
 }
+
+float D::get_v(){
+	return this->v;
+}
+
+void D::set_v(int v){
+	this->v = v;
+}
+
+void D::aumentar_v(int qtd){
+	this->v += qtd;
+}
+
 
 
 /**
@@ -87,29 +105,49 @@ class Aresta
 {
 	D vD;
 	F vF;
-	int peso;
+	float custo_atribuicao; // custo de atribuir o cliente vD a instalacao vF
+	float w; // parte do custo da instalacao, que o cliente vai contribuir
 public:
-	Aresta(D vD, F vF, int peso);
-	D getVD();
-	F getVF();
-	int getPeso();
+	Aresta(D vD, F vF, float custo_atribuicao);
+	D get_vD();
+	F get_vF();
+	float get_custo_atribuicao();
+	void set_custo_atribuicao(float custo_atribuicao);
+	float get_w();
+	void set_w(float w);
 };
 
-Aresta::Aresta(D _vD, F _vF, int peso) : vD(_vD.get_id()), vF(_vF.get_id(), _vF.get_custo_inst()){
-	this->peso = peso;
+Aresta::Aresta(D _vD, F _vF, float custo_atribuicao) : vD(_vD.get_id()), vF(_vF.get_id(), _vF.get_custo_inst()){
+	this->custo_atribuicao = custo_atribuicao;
+	this->w = 0; // inicialmente nao contribui com nada
 }
 
-D Aresta::getVD(){
+D Aresta::get_vD(){
 	return this->vD;
 }
 
-F Aresta::getVF(){
+F Aresta::get_vF(){
 	return this->vF;
 }
 
-int Aresta::getPeso(){
-	return this->peso;
+float Aresta::get_custo_atribuicao(){
+	return this->custo_atribuicao;
 }
+
+void Aresta::set_custo_atribuicao(float custo_atribuicao){
+	this->custo_atribuicao = custo_atribuicao;
+}
+
+float Aresta::get_w(){
+	return this->w;
+}
+
+void Aresta::set_w(float w){
+	this->w = w;
+}
+
+
+
 
 /**
 
@@ -124,8 +162,10 @@ class Grafo
 	vector<Aresta> arestas;
 public:
 	Grafo(int qtd_D, int qtd_F);
-	void adicionar_aresta(D vD, F vF, int peso);
+	void adicionar_aresta(D vD, F vF, float custo_atribuicao);
 	bool eh_vizinho(D vD, F vF);
+	vector<Aresta> get_arestas_d(int id_vD);
+	vector<Aresta> get_arestas();
 };
 
 Grafo::Grafo(int qtd_D, int qtd_F){
@@ -133,17 +173,30 @@ Grafo::Grafo(int qtd_D, int qtd_F){
 	this->qtd_F = qtd_F;
 }
 
-void Grafo::adicionar_aresta(D vD, F vF, int peso){
-	Aresta aresta(vD,vF,peso);
+void Grafo::adicionar_aresta(D vD, F vF, float custo_atribuicao){
+	Aresta aresta(vD,vF,custo_atribuicao);
 	arestas.push_back(aresta);
 }
 
 bool Grafo::eh_vizinho(D vD, F vF){
 	for(vector<Aresta>::iterator it = arestas.begin() ; it != arestas.end(); ++it){
-		if((it->getVD().get_id() == vD.get_id()) && (it->getVF().get_id() == vF.get_id()))
+		if((it->get_vD().get_id() == vD.get_id()) && (it->get_vF().get_id() == vF.get_id()))
 			return true;
 	}
 	return false;
+}
+
+vector<Aresta> Grafo::get_arestas_d(int id_vD){
+	vector<Aresta> arestas_D;
+	for(vector<Aresta>::iterator it = arestas.begin() ; it != arestas.end(); ++it){
+		if(it->get_vD().get_id() == id_vD)
+			arestas_D.push_back(*it);
+	}
+	return arestas_D;
+}
+
+vector<Aresta> Grafo::get_arestas(){
+	return this->arestas;
 }
 
 
@@ -183,7 +236,7 @@ Grafo popular_grafo(vector<D> cli, vector<F> inst){
 
 	for(int i=0;i<qtd_clientes;i++)
 		for(int j=0;j<qtd_instalacoes;j++)
-			g.adicionar_aresta(cli[i],inst[j], ((i+j)/2));
+			g.adicionar_aresta(cli[i],inst[j], (300 - i - j));
 
 	return g;
 }
@@ -202,6 +255,55 @@ int main (){
 	// Colocando no grafo os clientes e as instalações de forma a associar os custos de atribuição de maneira prática
 	Grafo grafo(qtd_clientes,qtd_instalacoes);
 	grafo = popular_grafo(cli,inst);
+
+	// v <- 0, w <- 0 ja acontece na propria inicializacao 
+
+	// Conjunto dos clientes que estamos aumentando as variaveis duais
+	vector<D> s;
+	s = cli; // inicia com todos os clientes
+
+	// Conjunto das instalacoes que possuem desigualdade justa
+	vector<F> t; // inicia vazio
+
+
+	// Grafo contendo somente os clientes ainda em s e todas as instalacoes
+	Grafo g(qtd_clientes,qtd_instalacoes);
+	g = grafo; // inicia com o grafo todo
+
+
+
+	// Aresta auxiliar, inicia com o valor da primeira aresta
+	Aresta menor = g.get_arestas()[0];
+
+
+	// Percorrer todas as arestas para ver qual o menor cij
+	for(vector<Aresta>::iterator it = (g.get_arestas().begin()+1) ; it != g.get_arestas().end(); ++it){
+		if(it->get_custo_atribuicao() < menor.get_custo_atribuicao()){
+			menor = *it;
+		}	
+	}
+
+
+	cout << "menor custo: " << menor.get_custo_atribuicao() << endl;
+
+	// Percorrer todos os clientes para aumentar em todos esse valor
+	for(vector<D>::iterator it = s.begin() ; it != s.end(); ++it){
+		s.aumentar_v(menorC.get_custo_atribuicao());
+	}
+
+
+	// Aresta auxiliar e variavel auxiliar, inicia com o valor da primeira aresta
+	menor = g.get_arestas()[0];
+	int menor_valor = menor.custo_atribuicao() + menor.w() - menor.get_vD().get_v();
+
+	// Percorrer todas as arestas para ver qual o menor cij + wij - vj (o q ja foi acrescentado anteriormente)
+	for(vector<Aresta>::iterator it = (g.get_arestas().begin()+1) ; it != g.get_arestas().end(); ++it){
+		if(it->get_arestas()[0].custo_atribuicao() + it->get_arestas()[0].w() - it->get_arestas()[0].get_vD().get_v() < menor_valor){
+			menor = it->get_arestas()[0];
+			menor_valor = menor.custo_atribuicao() + menor.w() - menor.get_vD().get_v();
+		}	
+	}
+
 
 
 	return 0;
