412bda890c1b9c280082858e351da51bc378fb36
Unknown <renatassmendes@hotmail.com>
2018-10-29 00:12:43 -0300

    Teoricamente segunda etapa caso A esta pronta

--- primalDualLemon.cpp
+++ primalDualLemon.cpp
@@ -189,6 +189,8 @@ int main(){
 
 
 
+	int tag_inst_aberta = false; // tag para ver se a inst está aberta (se está em T)
+	int menorAB; // receberá o menor valor entre o menor de A ou o menor de B
 
 	// ****** A partir daqui deve estar em um loop até nao ter mais clientes ativos:
 
@@ -199,6 +201,8 @@ int main(){
 
 	float qtd_menorA = 10000; // PROBLEMA: seria bom pegar o valor da primeira, ao inves de 10000
 	float qtd_atual;
+	int nome_inst_menorA = -1;
+	int nome_cli_menorA = -1;
 
 	for(ListGraph::NodeIt v(S); v != INVALID; ++v){
 		if (tipo[v]==0) { 			// se for cliente
@@ -208,6 +212,8 @@ int main(){
 					qtd_atual = custoAtribuicao[e] - label[v];
 					if(qtd_atual < qtd_menorA){
 						qtd_menorA = qtd_atual;
+						nome_inst_menorA = nome[S.u(e)]; // PROBLEMA: E SE TIVER MAIS QUE UMA INSTALACAO?
+						nome_cli_menorA = nome[S.v(e)]; //  PROBLEMA: E SE TIVER MAIS QUE UM CLIENTE?
 					}
 				}
 			}
@@ -221,6 +227,7 @@ int main(){
 	// B - fazer um for percorrendo todas as instalações para ver (fi - somatório de tudo que foi contribuído ate então para fi ) / numero de clientes prontos para contribuir para fi … os que ja alcançaram cij e ainda estão ativos
 
 	float qtd_menorB = 10000; // PROBLEMA: seria bom pegar o valor da primeira, ao inves de 10000
+	int nome_menorB = -1;
 
 	for(ListGraph::NodeIt v(S); v != INVALID; ++v){
 		if (tipo[v]==1) { 			// se for instalacao
@@ -238,13 +245,14 @@ int main(){
 					qtd_atual += w[e]; // vai indicar somatorio de todos w dessa instalacao
 				}
 
-				qtd_atual = (label[v] - qtd_atual)/cli_contrib_inst[indice_inst][0];
+				qtd_atual = (label[v] - qtd_atual)/cli_contrib_inst[indice_inst][0]; //(fi - somatório de tudo que foi contribuído ate então para fi ) / (numero de clientes prontos para contribuir para fi) 
 
 			}
 
 			cout << "qtd atualL " << qtd_atual << endl;
 			if(qtd_atual < qtd_menorB){
 				qtd_menorB = qtd_atual;
+				nome_menorB = nome[v]; // PROBLEMA: E SE TIVER MAIS QUE UMA INSTALACAO?
 			}
 		}
 	}
@@ -252,20 +260,100 @@ int main(){
 	cout << "temos a qtd menor na parte B: " << qtd_menorB << endl;
 
 
+	//	ver o menor entre A e B e aumentar esse valor em todos os clientes (tanto no cij se ainda faltar quanto no wij se ja estiver contribuindo)
+
+	if(qtd_menorA < qtd_menorB){
+		menorAB = qtd_menorA;
+	}
+	else if(qtd_menorB < qtd_menorA){
+		menorAB = qtd_menorB;
+	}
+	// SENAO: EMPATE
+	else{
+		cout << "Empatou! Ver o que fazer" << endl;
+		menorAB = qtd_menorA; // PROBLEMA: E SE DEU EMPATE? por enquanto ta assim
+	}
+
+	// Percorrer todos os clientes para aumentar em todos esse valor em v
+	for(ListGraph::NodeIt v(S); v != INVALID; ++v){
+		if (tipo[v]==0) { 			// se for cliente
+			label[v] += menorAB;
+		}
+	}
+
+	// Percorrer todas as arestas para aumentar em w, caso o cliente estivesse pronto para contribuir
+	// Aproveitar e ver quais bateram o custo de atribuicao
+	// Entao, acionar a flag prontoContribuirW e colocar no cli_contrib_inst
+	for(ListGraph::EdgeIt e(S); e!= INVALID; ++e){
+		if(prontoContribuirW[e]){ // se está pronto para contribuir
+			w[e] += menorAB; // PROBLEMA: será que não tem problema de aumentar esse valor varios vezes e estourar? Talvez dividir pelo numero de inst q ele contribui?
+		}
+		else if(custoAtribuicao[e] == label[S.v(e)]){ // SENAO SE: está pronto para contribuir (ja pagou o ca)
+			prontoContribuirW[e] = true;
+			indice_inst = nome[S.u(e)] - qtd_clientes;
+			cli_contrib_inst[indice_inst][0] += 1; // aumenta em 1 o numero de cliente que contribui para aquela inst
+			cli_contrib_inst[indice_inst][cli_contrib_inst[indice_inst][0]] = nome[S.v(e)]; // atribui esse novo cliente em sua lista
+		}
+	}
+
+
+
+	// REPETINDO O IF, PARA TRATAR DOS DETALHES AGORA
+
 	// SE FOR O CASO A: verificar se a instalação que aquele cliente alcançou ja estava aberta, se sim, remover ele dos ativos
 	if(qtd_menorA < qtd_menorB){
 		cout << "Caso A!" << endl;
+		tag_inst_aberta = false;
+		for(ListGraph::NodeIt v(T); v != INVALID; ++v){ // Percorrer por T para ver se o nome_inst_menorA está la (se a inst ja está aberta)
+			if(nomeT[v] == nome_inst_menorA){
+				tag_inst_aberta = true;
+				break;
+			}
+		}
+		if(tag_inst_aberta){ // se a instalacao ja estava aberta
+			cout <<"Instalacao ja estava aberta. Removendo o cliente em questao dos ativos "<< endl;
+			S.erase(S.nodeFromId(nome_cli_menorA)); 
+		}
 	}
 	// SE FOR O CASO B: caso B seja o menor valor: abrir a instalação i e remover os seus contribuintes dos clientes ativos (lembrando de remover eles das listas de contribuintes das outras instalações (talvez só fazer uma tag se está ativo ou não)
 	else if(qtd_menorA > qtd_menorB){ 
 		cout << "Caso B!" << endl;
-	}
+		instT[qtd_instT] = T.addNode();
+		f[instT[qtd_instT]] = label[S.nodeFromId(nome_menorB)]; // pega o valor fi da instalacao
+		nomeT[instT[qtd_instT]] = nome_menorB; // pega o nome da instalacao
+		qtd_instT += 1;
+
+		// FALTA O FINAL, APAGAR AS COISAS
+
+	} 
 	// SENAO: EMPATE
 	else{
 		cout << "Empatou! Ver o que fazer" << endl;
 	}
 
 
+	// // Percorrendo por todos os nós de T
+	cout<<"-- T --" << endl;
+	for(ListGraph::NodeIt v(T); v != INVALID; ++v){
+		cout << "no id: " << T.id(v) << " - nome: " << nomeT[v] << " - f: " << f[v] << endl;
+	}
+	cout<<"-------" << endl;
+
+
+
+	
+	// Percorrendo por todos os arcos
+	for(ListGraph::EdgeIt e(S); e!= INVALID; ++e){
+		cout << "arco id: " << S.id(e) ;
+		cout << " - cliente: " << S.id(S.v(e)) << " - instalacao: " << S.id(S.u(e));
+		cout<< " - ca: " << custoAtribuicao[e] << " w - " << w[e] << " PCW - " << prontoContribuirW[e] << endl;
+	}
+
+
+	// Percorrendo por todos os nós
+	for(ListGraph::NodeIt v(S); v != INVALID; ++v){
+		cout << "no id: " << S.id(v) << " - tipo: " << tipo[v] << " - nome: " << nome[v] << " - label: " << label[v] << endl;
+	}
 
 
 
