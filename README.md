9dea037a4f330117b1e51dbd9454c07198db3434
Unknown <renatassmendes@hotmail.com>
2019-09-30 00:52:59 -0300

    Gurobi: adapting the solution reader to get test cases from a list

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Codigos para resolver o problema da localizacao de instalacoes sem capacidade
 
 ## Conteudo das pastas 
-- A pasta gurobi contem os arquivos relacionados a resolucao do programa linear inteiro, misto e fracionario com Gurobi.
+- A pasta gurobi contem os arquivos relacionados a resolucao do programa linear inteiro, misto e fracionario com Gurobi. Ela também contem a resolucao do programa linear do Dual do problema.
 - A pasta primalDual contem os arquivos relacionados a resolucao aproximada do problema, utilizando o metodo Primal Dual.
 - A pasta guloso contem os arquivos relacionados a resolucao aproximada do problema, utilizando o metodo Guloso.
+- A pasta localSearch contem os arquivos relacionados a resolucao aproximada do problema, utilizando o metodo de Busca Local.
 - A pasta baseDeTestes contem diversos casos de teste, com suas respectivas solucoes otimas.
-- O arquivo solutions.txt contem a analise e comparacao dos resultados
+- O arquivo solutions.pages contem a analise e comparacao dos resultados
 
 - OBS: Para criar novas instancias pode-se utilizar o script.py. Para isso, considere qtd_inst como a quantidade de instalacoes que deseja, qtd_cli como a quantidade de clientes, arquivo.txt como o arquivo no qual deseja que a instancia seja criada, então basta digitar no terminal:
 
--- gurobi/leitorSol.py
+++ gurobi/leitorSol.py
@@ -33,15 +33,28 @@ if __name__ == '__main__':
 
 		if ok:
 
-			files_test = os.listdir('solutions' + formulation_type)
-			files_test.sort()
+			# If you want to list your test cases
+			file_location = 'testCasesReader.txt'
+
+			with open(file_location, 'r') as input_list_data_file:
+			    input_list_data = input_list_data_file.read()
+
+			files_test = input_list_data.split()
+			# ---- end if ----- #
+
+			# Else if you want to test everything you have and sort the result
+			# files_test = os.listdir('solutions' + formulation_type)
+			# files_test.sort()
+			# IMPORTANT: IF YOU CHOOSE THIS CASE, YOU NEED TO CHANGE THE complete_file_name AND THE file_name
+			# ---- end else if ----- #
 
 			solution_list = []
 			for file_name in files_test:
 				if file_name != ".DS_Store": # ignore this file
 					print("Input file:", file_name)
 
-					complete_file_name = 'solutions' + formulation_type + '/' + file_name
+					complete_file_name = 'solutions' + formulation_type + '/' + file_name + ".sol"
+					# complete_file_name = 'solutions' + formulation_type + '/' + file_name
 					with open(complete_file_name, 'r') as input_data_file:
 						input_data = input_data_file.read()
 
@@ -50,7 +63,8 @@ if __name__ == '__main__':
 						solution = input_data.split(" ")
 
 						if len(solution) >= 3: # checking if the solution is in a correct format
-							solution_list.append((file_name[:-4],solution[0],solution[1],solution[2])) #deleting ".sol" from filename
+							solution_list.append((file_name,solution[0],solution[1],solution[2]))
+							# solution_list.append((file_name[:-4],solution[0],solution[1],solution[2])) #deleting ".sol" from filename
 
 			# Exporting csv 
 			export_csv("solutions" + formulation_type + ".csv",solution_list)
