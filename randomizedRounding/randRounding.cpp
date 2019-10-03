9f3baf796fb0fdf06d1dd9564dfe843608d49563
Unknown <renatassmendes@hotmail.com>
2019-10-03 03:40:48 -0300

    RR: algorithm done!!

--- randomizedRounding/randRounding.cpp
+++ randomizedRounding/randRounding.cpp
@@ -1,15 +1,9 @@
 #include <lemon/list_graph.h>
 #include <algorithm>
 #include <iostream>
-#include <set>
-#include <iterator>
 #include <ctime>
 #include "definitions.hpp"
 #define EPSL 0.00000001
-
-// Observacao importante: ao usar g.nodeFromId() eh necessario passar o ID geral, nao o blue ID nem red ID. 
-// No caso do cliente, o red ID = ID geral, pois os clientes foram os primeiros a serem adicionados no grafo.
-// No caso da instalacao, o ID geral = blue ID + qty_clients, pois as instalacoes foram adicionadas no grafo logo apos todos os clientes.
 
 using namespace lemon;
 using namespace std;
@@ -18,7 +12,12 @@ using namespace std;
 #define DISPLAY_TIME 2 // corresponde aos calculos de tempo 
 #define DISPLAY_GRAPH 3 // corresponde a descricao dos clientes, instalacoes e arcos
 
-#define DEBUG 0 // OPCOES DE DEBUG: 1 PARA EXIBIR OS MOVIMENTOS REALIZADOS, 2 PARA EXIBIR ACOES, 3 PARA EXIBIR TEMPO, 4 PARA EXIBIR AS MUDANÇAS NO GRAFO
+#define DEBUG 0 // OPCOES DE DEBUG: 1 PARA EXIBIR ACOES, 2 PARA EXIBIR TEMPO, 3 PARA EXIBIR AS MUDANÇAS NO GRAFO
+
+
+double randZeroToOne(){
+	return ((double) rand() / (RAND_MAX));
+}
 
 
 void mergeSort(double *vetor, int *vetorID, int posicaoInicio, int posicaoFim) {
@@ -85,6 +84,9 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 	cout << fixed;
    	cout.precision(5);
 
+   	// Semente do numero aleatorio
+   	srand((unsigned)time(NULL));
+
 	// Struct que vai retornar a solução
 	solutionType solution;
 
@@ -92,7 +94,13 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 	solution.qty_clients = qty_clients;
 	solution.finalTotalCost = 0;
 	solution.timeSpent = 0;
-	////// OBS: solution.assigned_facilities não está inicializado, ver se precisaria
+
+	// indicara as instalacoes atribuidas a cada cliente na resposta
+	solution.assigned_facilities = (int*) malloc((qty_clients) * sizeof(int));
+	if(!solution.assigned_facilities){
+		cout << "Memory Allocation Failed";
+		exit(1);
+	}
 
 	/* Inicio declaracoes variaveis para calculo de tempo */
 
@@ -105,7 +113,7 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 	/* Fim declaracoes para calculo de tempo */
 
 	if(DEBUG >= DISPLAY_TIME){
-		// INICIANDO A CONTAGEM DE TEMPO DA FUNCAO COMO UM TODO
+		// Iniciando a contagem de tempo da funcao como um todo
 		clock_gettime(CLOCK_REALTIME, &real_start);
 	}
 
@@ -119,12 +127,6 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 	// Grafo que liga os clientes às instalações
 	ListBpGraph g;
 
-	// c_minX - será correnpondente a c(j,X) = min_{i \in X} cij, sendo X as inst abertas
-	ListBpGraph::RedNodeMap<double> c_minX(g);
-
-	// nearest_open_fac - será correnpondente ao ID da instalacao i tal que min_{i \in X} cij, sendo X as inst abertas
-	ListBpGraph::RedNodeMap<int> nearest_open_fac(g);
-
 	// v_C - será correnpondente a vj* + Cj* , sendo Cj* = sum_{i \in F} (cij xij*)
 	ListBpGraph::RedNodeMap<double> v_C(g);
 
@@ -136,12 +138,6 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 
 	// open - Indica se a instalação está aberta ou não
 	ListBpGraph::BlueNodeMap<bool> open(g);
-
-	// active - indica se o cliente está ativo ainda ou nao, isto é, se ele ainda nao foi conectado a nenhuma instalacao aberta. if j \in g
-	ListBpGraph::RedNodeMap<bool> active(g);
-
-	// name - Para identificar cada nó
-	ListBpGraph::NodeMap<int> name(g);
 
 
 	// sorted_vC - será os clientes ativos ordenados pelo v_C 
@@ -155,9 +151,6 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
         exit(1);
     }
 
-    // cur_i_sorted_vC - indica qual o proximo indice do vetor sorted_vC devo tentar usar (que ainda nao foi usado)
-    int cur_i_sorted_vC = 0;
-
 
 	// Criação de nós de instalações e atribuição de seus labels
 	ListBpGraph::BlueNode * facilities;
@@ -167,14 +160,11 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 		exit(1);
 	}
 
-
 	for(int i=0;i<qty_facilities;i++){
 		facilities[i] = g.addBlueNode();
 		f[facilities[i]] = costF[i]; // pegando valor vindo por parametro
-		name[facilities[i]] = qty_clients + i; // nomeia de acordo com a numeracao
 		open[facilities[i]] = false; // indica que a instalação não está aberta inicialmente
 	}
-
 
 
 	// Criação de nós clientes e atribuição de seus labels
@@ -187,10 +177,6 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 
 	for(int i=0;i<qty_clients;i++){
 		clients[i] = g.addRedNode();
-		name[clients[i]] = i; // nomeia de acordo com a numeracao
-		nearest_open_fac[clients[i]] = -1; // indica que não está conectado com nenhuma inst inicialmente 
-		c_minX[clients[i]] = -1; // indica que nao está conectado com ninguém inicialmente //PROBLEMA: nao tenho ctz ainda qual a melhor inicializacao
-		active[clients[i]] = true;
 		v_C[clients[i]] = v_values[i]; // inicia com o valor de vj
 		index_vC[clients[i]] = -1; // inicialmente nao está em nenhuma posicao na ordenacao
 	}
@@ -229,8 +215,10 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 		cout << "Now we have the array sorted_vC sorted"<< endl;
 	}
 
+	// Salvando o index de cada cliente no vetor sorted_vC em seu node map
 	for(int i=0;i<qty_clients;i++){
 		index_vC[clients[sorted_vC_ID[i]]] = i;
+
 		if (DEBUG >= DISPLAY_ACTIONS){
 			cout << "Client " << sorted_vC_ID[i] << ": " << sorted_vC[i] << endl;  
 		}
@@ -241,22 +229,21 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 		// Percorrendo por todos os nós A - clientes
 		cout << "Scrolling through all clients" << endl;
 		for(ListBpGraph::RedNodeIt n(g); n != INVALID; ++n){
-			cout << "node id: " << g.id(n)  << " - name: " << name[n];
-			cout << " - active: " << active[n] << " - v_C: " << v_C[n];
+			cout << "node id: " << g.id(n) << " - v_C: " << v_C[n];
 			cout << " - index_vC: " << index_vC[n] << endl;
 		}
 
 		// Percorrendo por todos os nós B - instalacoes
 		cout << "Scrolling through all facilities" << endl;
 		for(ListBpGraph::BlueNodeIt n(g); n != INVALID; ++n){
-			cout << "node id: " << g.id(n)  << " - name: " << name[n] << " - f: " << f[n] << " - open: " << open[n] << endl;
+			cout << "node id: " << g.id(n) << " - f: " << f[n] << " - open: " << open[n] << endl;
 		}
 
 		// Percorrendo por todos os arcos
 		cout << "Scrolling through all edges" << endl;
 		for(ListBpGraph::EdgeIt e(g); e!= INVALID; ++e){
 			cout << "edge id: " << g.id(e) ;
-			cout << " - client: " << name[g.u(e)] << " - facility: " << name[g.v(e)];
+			cout << " - client: " << g.id(g.u(e)) << " - facility: " << g.id(g.v(e));
 			cout<< " - ac: " << assignment_cost[e] << endl;
 		}
 
@@ -282,10 +269,14 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 
 
 	int qty_active_clients = qty_clients; // Indica a quantidade de clientes ainda em g ativos.
+    int cur_i_sorted_vC = 0; // indica qual o proximo indice do vetor sorted_vC devo tentar usar (que ainda nao foi usado)
+	int cur_client = -1; // indica qual cliente escolhemos nessa iteracao atual
+	int cur_facility = -1; // indica qual instalacao escolhemos nessa iteracao atual, vizinha de cur_client
+	double rand_value; // indica o valor gerado randomicamente naquela iteracao 
+	double sum_xij; // indica a soma dos xij ate encontrar a instalacao correspondente sorteada
 
 
 	// ****** A partir daqui deve estar em um loop até todos os clientes se conectarem a alguma instalacao
-
 
 	while(qty_active_clients > 0){
 
@@ -293,15 +284,107 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 			cout << endl << "------------------------------ WE STILL HAVE " << qty_active_clients << " ACTIVE CLIENTS ------------------------------" << endl << endl;
 		}
 
-		// VOU APAGAR ISSO, COLOCANDO SO PRA FINS DE TESTE
-		qty_active_clients -= 1;
+		/*
+		Escolha jk \in C que minimize vj* + Cj* dentre todos j \in C
+		*/
+
+		while((cur_i_sorted_vC < qty_clients ) && (sorted_vC[cur_i_sorted_vC] < 0)){ // segunda condicao indica que era -1, q significa que o cliente nao está ativo mais
+			cur_i_sorted_vC += 1; // entao vamos tentar o proximo
+		}
+
+		// Se acabaram os clientes, devemos dar break (nao era pra entrar aqui nunca)
+		if(cur_i_sorted_vC >= qty_clients){
+			if(DEBUG >= DISPLAY_ACTIONS){
+				cout << "Weird value of cur_i_sorted_vC, it's greater than qty_clients" << endl;
+			}
+			break;
+		}
+
+		// Pegando o primeiro cliente ok
+		cur_client = sorted_vC_ID[cur_i_sorted_vC];
+
+		if(DEBUG >= DISPLAY_ACTIONS){
+			cout << "Current client: " << cur_client << endl;
+		}
+
+
+		/*
+		Escolha ik \in N(jk) de acordo com a distribuição probabilística xij*
+		*/
+
+		rand_value = randZeroToOne();
+
+		if(DEBUG >= DISPLAY_ACTIONS){
+			cout << "rand value = " << rand_value << endl;
+		}
+
+		cur_facility = 0;
+		sum_xij = 0;
+
+		// Encontrando qual a instalacao correspondente ao numero sorteado
+		while(cur_facility < qty_facilities){
+			sum_xij += x_values[cur_facility][cur_client];
+			if(sum_xij > rand_value){
+				break;
+			}
+			cur_facility += 1;
+		}
+
+		if(DEBUG >= DISPLAY_ACTIONS){
+			cout << "sum = " << sum_xij << endl;
+			cout << "Chosen facility = " << cur_facility << endl;
+		}
+
+		/*
+		F' <- F' U {ik}
+		*/
+
+		// Abrindo a instalacao escolhida
+		if(!open[facilities[cur_facility]]){
+
+			if(DEBUG >= DISPLAY_ACTIONS){
+				cout << "Opening facility " << cur_facility << endl;
+			}
+
+			open[facilities[cur_facility]] = true;
+			solution.finalTotalCost += f[facilities[cur_facility]];
+		}
+
+		/*
+		Atribua jk e todos os clientes não atribuídos em N2(jk) a ik
+		*/
+
+		for(int j=0;j<qty_clients;j++){
+			if(x_values[cur_facility][j] > 0){   // Se esse cliente for vizinho de cur_facility, vamos atribui-lo
+
+				if(DEBUG >= DISPLAY_ACTIONS){
+					cout << "Assigning client " << j << " to facility " << cur_facility << endl;
+				}
+
+				solution.assigned_facilities[j] = cur_facility;
+				solution.finalTotalCost += assignment_cost[findEdge(g, clients[j], facilities[cur_facility])];
+
+				/*
+				C <- C - jk - N2(jk)
+				*/
+
+				// Desativando cliente
+				sorted_vC[index_vC[clients[j]]] = -1;
+				qty_active_clients -= 1;
+
+				// Zerando todos os valores de x_values correspondentes a esse cliente
+				for(int i=0;i<qty_facilities;i++){
+					x_values[i][j] = 0;
+				}
+			}
+		}
 	}
 
 	cout << "FINAL TOTAL COST: " << solution.finalTotalCost << endl;
 
 	if(DEBUG >= DISPLAY_TIME){
 
-		// FINALIZANDO A CONTAGEM DE TEMPO DA FUNCAO COMO UM TODO
+		// Finalizando a contagem de tempo da funcao como um todo
 		clock_gettime(CLOCK_REALTIME, &real_finish);
 
 		// Calculando o tempo gasto total
@@ -313,6 +396,8 @@ solutionType randRounding(int qty_facilities, int qty_clients, double * costF, d
 	free(clients);
 	free(facilities);
 	free(edges);
+	free(sorted_vC);
+	free(sorted_vC_ID);
 
 	return(solution);
 }
