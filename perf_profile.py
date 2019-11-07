e8d1bc18b9684cd97fb3c8ad70bfff290b7565af
Unknown <renatassmendes@hotmail.com>
2019-11-08 10:40:45 +1100

    Translating the performance profile comments

--- perf_profile.py
+++ perf_profile.py
@@ -8,67 +8,66 @@ def get_solutions(file_location,initial_sol_rr):
 		input_csv_data = input_csv_data_file.read()
 
 	files_test = input_csv_data.split("\n")
-	del(files_test[0]) #deletando a linha que contem os nomes das colunas
+	del(files_test[0]) # deleting the row containing the column names
 
 	solution_list = []
 
-	if not initial_sol_rr: # se a solução inicial não vier do RR
+	if not initial_sol_rr: # if the initial solution does not come from RR
 		for file in files_test:
 			elements = file.split(",")
 
-			if len(elements) > 1 : # para nao contar alguma linha vazia que possa ter
-				solution_list.append(elements[1]) #pega o custo da solução
+			if len(elements) > 1 : # not to count any empty lines you might have
+				solution_list.append(elements[1]) # get the cost of the solution
 
-	else: # se a solução inicial vier do RR
+	else: # if the initial solution comes from RR
 		for file in files_test:
 			elements = file.split(",")
 
-			if len(elements) > 1 : # para nao contar alguma linha vazia que possa ter
-				solution_list.append(elements[3]) #pega a média do custo da solução
+			if len(elements) > 1 : # not to count any empty lines you might have
+				solution_list.append(elements[3]) # get the average solution cost
 
 	return solution_list
 
 
 def calculate_best(solutions):
-	best = [] #armazena o melhor valor pra cada instancia
+	best = [] # stores the best value for each instance
 
-	for i in range(len(solutions[0])): #percorre por cada instancia
-		best.append(float("inf")) # inicia com um valor infinito
+	for i in range(len(solutions[0])): # scroll through each instance
+		best.append(float("inf")) # starts with an infinite value
 		
-		for alg in solutions: #percorre pelos tipos de algoritmo
-			if float(alg[i]) < best[-1]: # atualiza o valor da melhor solucao dessa instancia
+		for alg in solutions: # scroll through algorithm types
+			if float(alg[i]) < best[-1]: # updates the value of the best solution for this instance
 				best[-1] = float(alg[i])
 
 	return best
 
 
 def calculate_distances(solutions,best_solutions):
-	distances = [] #  é uma matrix.. O primeiro vetor contem len(solutions) elementos. Cada um desses elelementos contem len(solutions[0]) elementos e armazena a distancia de cada instancia da melhor solucao (em %)
-
-	for alg in solutions: #percorre pelos tipos de algoritmo
+	distances = [] # It is a matrix. The first vector contains len(solutions) elements. Each of these elements contains len(solutions[0]) elements and stores the distance of each instance of the best solution (in %).
+	for alg in solutions: # scroll through algorithm types
 		distances.append([])
 
-		for i in range(len(solutions[0])): #percorre por cada instancia
+		for i in range(len(solutions[0])): # scroll through each instance
 			sub = float(alg[i]) - best_solutions[i]
-			d = (sub * 100) / best_solutions[i] # calcula a porcentagem da distancia
+			d = (sub * 100) / best_solutions[i] # calculate percentage of distance
 			distances[-1].append(d)
 
-		distances[-1].sort() # ordena as distancias
+		distances[-1].sort() # sort the distances
 
 	return distances
 
 
 def prepare_graphic(x_values,solutions, distances):
-	y_values = [] # é uma matrix. O primeiro vetor contem len(solutions) elementos. Cada um desses elementos contem len(x_values) elementos
+	y_values = [] # It is a matrix. The first vector contains len(solutions) elements. Each of these elements contains len(x_values) elements
 
-	for alg in range(len(solutions)): #percorre pelos tipos de algoritmo
+	for alg in range(len(solutions)): # scroll through algorithm types
 		qty = 0
 		y_values.append([])
-		for x in x_values: # percorre pelos pontos do eixo x (% distancia da melhor solucao)
-			while (qty < len(solutions[0])) and (distances[alg][qty] <= x): # enquanto nao tiver acabado o vetor e a distancia for menor que o x
+		for x in x_values: # scroll through x-axis points (% distance from best solution)
+			while (qty < len(solutions[0])) and (distances[alg][qty] <= x): # until the vector is finished and the distance is less than x
 				qty += 1
 
-			y = (qty * 100)/len(solutions[0]) # calculando a porcentagem da quantidade
+			y = (qty * 100)/len(solutions[0]) # calculating the percentage of quantity
 			y_values[-1].append(y)
 
 	return y_values
@@ -76,12 +75,12 @@ def prepare_graphic(x_values,solutions, distances):
 
 if __name__ == '__main__':
 
-	ZOOM = -1 # Usado caso queira dar um zoom no grafico, ignorando o maximo valor encontrado na solucao. obs: utilizar -1 caso nao queira dar zoom ou colocar o valor do maximo desejado
+	ZOOM = -1 # Used if you want to zoom the graph, ignoring the maximum value found in the solution. Note: use -1 if you do not want to zoom or set the desired maximum value
 
-	solutions = [] #vai armazenar as soluces de cada algoritmo
-	legend = [] #vai indicar a ordem da legenda no grafico
+	solutions = [] # it will store the solutions of each algorithm
+	legend = [] # it will indicate the order of the caption on the graph
 
-	# Pegando as informacoes das solucoes do csv de cada algoritmo
+	# Getting csv solution information from each algorithm
 	solutions.append(get_solutions("gurobi/solutionsTrivial.csv", False))
 	legend.append("Trivial")
 	solutions.append(get_solutions("greedy/solutions.csv", False))
@@ -99,58 +98,58 @@ if __name__ == '__main__':
 	solutions.append(get_solutions("localSearch/solutionsDual.csv",False))
 	legend.append("LS - PD")
 
-	# Salvando a melhor solucao de cada instancia
+	# Saving the best solution for each instance
 	best_solutions = calculate_best(solutions)
 
-	# Calculando a distancia da solucao de cada algoritmo com a melhor solucao de cada instancia
+	# Calculating the distance of each algorithm solution with the best solution of each instance
 	distances = calculate_distances(solutions,best_solutions)
 
 	"""
-	Verificar a menor e a maior distancia
+	Check the smallest and the biggest distance
 	"""
 
-	# Iniciam com valores invalidos
+	# Start with invalid values
 	minimum = float("inf")
 	maximum = -1
 
-	# percorre pelos tipos de algoritmo
+	# scroll through algorithm types
 	for alg in distances: 
 		i = 0
-		while(alg[i]==0): # Garante que o minimum nao sera 0
+		while(alg[i]==0): # Guarantees that the minimum will not be 0
 			i+=1
 
-		# Salvando a maior e a menor distancia encontrada
+		# Saving the smallest and the biggest distance found
 		if (i < len(alg)) and (alg[i] < minimum):
 			minimum = alg[i]
 		if (ZOOM < 0) and (alg[-1] > maximum):
 			maximum = alg[-1]
 
-	# caso tudo seja igual a 0, o passo sera 1
+	# if everything equals 0, the step will be 1
 	if minimum == float("inf"):
 		minimum = 1
-	# caso o minimo seja muito pequeno, mudamos por razoes de capacidade de memoria
+	# if the minimum is too small, we change for reasons of memory capacity
 	elif minimum < 0.01:
 		minimum = 0.01
 
-	# Caso queira ignorar o maximum real e dar um zoom no grafico
+	# If you want to ignore the real maximum and zoom the graph
 	if ZOOM >= 0:
 		maximum = ZOOM
 
-	""" Fim verificar menor distancia """
+	""" End of checking the smallest and the biggest distance """
 
 	"""
-	Construir o eixo x:
-	- inicio = 0
-	- fim = maximum
-	- passo = minimum
+	build the x axis:
+	- start = 0
+	- end = maximum
+	- step = minimum
 	"""
 	x_values = []
 	for i in numpy.arange(0,maximum+minimum,minimum):
 		x_values.append(i)
 		
-	""" Fim construir o eixo x """
+	""" End of building the x axis """
 
-	# Preparando os valores de y do grafico
+	# Preparing the graph's y values
 	y_values = prepare_graphic(x_values,solutions,distances)
 
 	# naming the x axis 
@@ -161,7 +160,7 @@ if __name__ == '__main__':
 	# giving a title to my graph 
 	plt.title('Performance Profile UFL') 
 
-	# Plotando as linhas de cada algoritmo
+	# Plotting the lines of each algorithm
 	for i in range(len(solutions)):
 		plt.plot(x_values,y_values[i],label=legend[i])
 
