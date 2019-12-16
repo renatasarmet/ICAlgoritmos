63cdfd769a8f97a379cc9d0cd5957b8c0f61a363
Renata Sarmet <renatassmendes@hotmail.com>
2019-12-17 10:51:27 +1100

    Show groups clients

--- memetic/show_groups.py
+++ memetic/show_groups.py
@@ -15,15 +15,28 @@ if __name__ == '__main__':
 	size = 500 #qtd de clientes e instalacoes
 
 
+	cli_cli = [] #matriz cliente x cliente
+	for i in range(size):
+		cli_cli.append([0] * size)
+
+
+	cont = 0
+	vector = [] #[file_test][clients][facilities]
+	vector.append([]) # cont = 0
+	vector.append([]) # cont = 1
+
+	nearest_fac = [] #vetor de clientes (cont 0 e 1)
+	nearest_fac.append([])
+	nearest_fac.append([])
+
 	for file_name in files_test:
 		if file_name != ".DS_Store": # ignore this file
 			print("Input file:", file_name)
 
 			open_fac = [0] * size
 
-			vector = []
 			for i in range(size):
-				vector.append([0] * size)
+				vector[cont].append([0] * size)
 
 			complete_file_name = 'groups/' + file_name + ".sol"
 
@@ -36,11 +49,13 @@ if __name__ == '__main__':
 
 				if len(solution) >= size+1: # checking if the solution is in a correct format
 					for i in range(0,size):
-						vector[i][int(solution[i+2])] = 1
+						vector[cont][i][int(solution[i+2])] = 1
 						open_fac[int(solution[i+2])] = 1
+						nearest_fac[cont].append(int(solution[i+2]))
 				else:
 					print("missing info in solution")
 
+				print("Solution value: ", solution[0])
 				# mostrar instalacoes abertas
 				print("open fac:", end=" ")
 				for i in range(size):
@@ -52,11 +67,65 @@ if __name__ == '__main__':
 				for i in range(size):
 					used = False
 					for j in range(size):
-						if vector[j][i] == 1:
+						if vector[cont][j][i] == 1:
 							print(j, end=" ")
 							used = True
 					if used:
 						print("<-- fac: ", i, end="\n\n")
+
+			cont += 1
+
+
+	# Verificando a semelhanca
+	for i in range(size): #para cada cliente
+		for j in range(size): #para cada outro cliente
+			if vector[0][j][nearest_fac[0][i]] and vector[1][j][nearest_fac[1][i]]: # se o cliente estava atribuído à mesma instalacao em ambos os casos
+				if nearest_fac[0][i] == nearest_fac[1][i]: # se era exatamente a mesma instalacao
+					cli_cli[i][j] = 2
+				else:
+					cli_cli[i][j] = 1
+
+
+	# exibindo detalhes
+	# for i in range(size): #para cada cliente
+	# 	print("cli ", i, end=": ")
+	# 	for j in range(size): #para cada outro cliente
+	# 		if i != j:
+	# 			if cli_cli[i][j] == 1:
+	# 				print(j, end=" ")
+	# 			if cli_cli[i][j] == 2:
+	# 				print("(",j,"->",nearest_fac[0][i],")", end=" ")
+	# 	print()
+
+
+	# lista de todos os clientes
+	list_cli = []
+	for i in range(size):
+		list_cli.append(i)
+
+	print()
+	print("Groups:")
+	# exibindo os grupos
+	for i in range(size): #para cada cliente
+		used = False
+		same_fac = False
+		for j in range(size): #para cada outro cliente
+			if j in list_cli:
+				if cli_cli[i][j] >= 1:
+					print(j, end=" ")
+					list_cli.remove(j)
+					used = True
+					if cli_cli[i][j] == 2:
+						same_fac = True
+					elif same_fac:
+						print("*****ERROR")
+		if same_fac:
+			print(" ***** always facility -> ",nearest_fac[0][i], " ******** ")
+		elif used:
+			print()
+
+
+
 
 
 
