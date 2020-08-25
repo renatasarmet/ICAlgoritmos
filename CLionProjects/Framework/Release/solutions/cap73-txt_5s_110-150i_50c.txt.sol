823f594bfa41366f98da514add29e24837367bcf
Renata Sarmet <renatassmendes@hotmail.com>
2020-08-25 11:09:56 -0300

    Adding solutions cap71 to 134 evfw

---
+++ CLionProjects/Framework/Release/solutions/cap73-txt_5s_110-150i_50c.txt.sol
@@ -0,0 +1,58 @@
+Reading instance ../createInstance/generatedInstances/cap73-txt_5s_110-150i_50c.txt
+Finished instance reading
+Number of resources = 16
+Number of scenarios = 5
+Probabilities of Scenarios: (0) 0.21817 | (1) 0.05828 | (2) 0.16017 | (3) 0.26486 | (4) 0.29852 | 
+Scenario 0: 23977.6 26241.5 23201.1 23287.4 25224.7 21364.9 25844.8 20406.6 19599.2 22581.6 0 22220.3 23376 23451.6 19875.2 20713.3
+Scenario 1: 22535.4 21699.9 19983.9 20249.7 21067.6 20399 24096.2 24726.7 23899.5 21921.4 0 25115.9 19266.1 24393.9 22213.4 19388.6
+Scenario 2: 24250.6 22773.9 24773 23791.8 24946.9 21772.8 21442.2 22593 22232.2 21710.1 0 20838 25304.8 22929.1 22226.1 24346.4
+Scenario 3: 22619.9 19678.8 23747 20622.9 20801 25862.8 21852.8 21705.4 22500.1 21944.2 0 21403.8 20499.3 22857.2 25103.1 20630.2
+Scenario 4: 24801.4 26129.6 26215.3 23269.5 24982.1 25455.2 23468.9 23518.1 24104.3 21287.2 0 23843.1 19980.6 19777.4 20558.2 25611.5
+Average weighted inflation = nan
+Largest inflation = 0.499514
+Alpha = 0.4
+MAX_ITERATIONS_SINCE_LAST_IMPROVEMENT 2
+MAX_ITERATIONS_SINCE_BEST_SOLUTION 3
+NUMBER_OF_GENERATIONS 25
+POPULATION_SIZE 25
+TAIL_GENERATIONS_FACTOR 3
+Creating solution instance.
+Starting Evolutive Framework.
+
+** Determining the upper bound **
+Upper bound = 476913.95403
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+Local search solution cost = 471980.40153
+Local search time = 0.20701
+0.20704 local search - EF main loop cycle 0 solution cost 471980.40153
+ local search - EF main loop cycle 0 fixed resourced: 
+2(cost: 17500.00000) 10(cost: 0.00000) 12(cost: 17500.00000) 7.64588s Cost of SP solution after second stage solver 471980.40153
+
+** Determining the upper bound **
+Upper bound = 476913.95403
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+Local search solution cost = 471980.40153
+Local search time = 0.20551
+7.85145 local search - EF main loop cycle 1 solution cost 471980.40153
+ local search - EF main loop cycle 1 fixed resourced: 
+2(cost: 17500.00000) 10(cost: 0.00000) 12(cost: 17500.00000) 16.02163s Cost of SP solution after second stage solver 471980.40153
+
+** Determining the upper bound **
+Upper bound = 476913.95403
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+Local search solution cost = 471980.40153
+Local search time = 0.21120
+16.23289 local search - EF main loop cycle 2 solution cost 471980.40153
+ local search - EF main loop cycle 2 fixed resourced: 
+2(cost: 17500.00000) 10(cost: 0.00000) 12(cost: 17500.00000) 23.87981s Cost of SP solution after second stage solver 471980.40153
+Number of cycles 3
+Time for each cycle 7.95995
+23.87985s current_cycle 3 | best_solution_cycle 0 | last_improvement_cycle 0
+Running the tail step
+44.60595s Cost of SP solution after tail step 471980.40153
+
+@Final_time 44.60761
+@Final_solution_cost 471980.40153
---
+++ CLionProjects/Framework/Release/solutions/cap74-txt_5s_110-150i_50c.txt.sol
@@ -0,0 +1,58 @@
+Reading instance ../createInstance/generatedInstances/cap74-txt_5s_110-150i_50c.txt
+Finished instance reading
+Number of resources = 16
+Number of scenarios = 5
+Probabilities of Scenarios: (0) 0.21817 | (1) 0.05828 | (2) 0.16017 | (3) 0.26486 | (4) 0.29852 | 
+Scenario 0: 34253.7 37487.8 33144.4 33267.7 36035.3 30521.3 36921.2 29152.3 27998.9 32259.5 0 31743.2 33394.3 33502.3 28393.2 29590.5
+Scenario 1: 32193.5 30999.8 28548.5 28928.1 30096.6 29141.4 34423.2 35323.9 34142.1 31316.3 0 35879.8 27523 34848.4 31733.4 27698
+Scenario 2: 34643.8 32534.1 35390 33988.3 35638.5 31104 30631.7 32275.6 31760.3 31014.5 0 29768.6 36149.7 32755.8 31751.6 34780.6
+Scenario 3: 32314.1 28112.6 33924.2 29461.2 29715.7 36946.9 31218.3 31007.7 32142.9 31348.8 0 30576.8 29284.7 32653.1 35861.5 29471.7
+Scenario 4: 35430.6 37328 37450.4 33242.1 35688.8 36364.5 33527 33597.3 34434.8 30410.3 0 34061.6 28543.7 28253.4 29368.8 36587.8
+Average weighted inflation = nan
+Largest inflation = 0.499514
+Alpha = 0.4
+MAX_ITERATIONS_SINCE_LAST_IMPROVEMENT 2
+MAX_ITERATIONS_SINCE_BEST_SOLUTION 3
+NUMBER_OF_GENERATIONS 25
+POPULATION_SIZE 25
+TAIL_GENERATIONS_FACTOR 3
+Creating solution instance.
+Starting Evolutive Framework.
+
+** Determining the upper bound **
+Upper bound = 487900.30893
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+Local search solution cost = 486980.40153
+Local search time = 0.20475
+0.20478 local search - EF main loop cycle 0 solution cost 486980.40153
+ local search - EF main loop cycle 0 fixed resourced: 
+2(cost: 25000.00000) 10(cost: 0.00000) 12(cost: 25000.00000) 7.58882s Cost of SP solution after second stage solver 486980.40153
+
+** Determining the upper bound **
+Upper bound = 487900.30893
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+Local search solution cost = 486980.40153
+Local search time = 0.19756
+7.78645 local search - EF main loop cycle 1 solution cost 486980.40153
+ local search - EF main loop cycle 1 fixed resourced: 
+2(cost: 25000.00000) 10(cost: 0.00000) 12(cost: 25000.00000) 15.08105s Cost of SP solution after second stage solver 486980.40153
+
+** Determining the upper bound **
+Upper bound = 487900.30893
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+ 0(0.50000)  1(0.50000)  2(0.50000)  3(0.50000)  4(0.50000)  5(0.50000)  6(0.50000)  7(0.50000)  8(0.50000)  9(0.50000)  10(0.50000)  11(0.50000)  12(0.50000)  13(0.50000)  14(0.50000)  15(0.50000) 
+Local search solution cost = 486980.40153
+Local search time = 0.19728
+15.27839 local search - EF main loop cycle 2 solution cost 486980.40153
+ local search - EF main loop cycle 2 fixed resourced: 
+2(cost: 25000.00000) 10(cost: 0.00000) 12(cost: 25000.00000) 22.54980s Cost of SP solution after second stage solver 486980.40153
+Number of cycles 3
+Time for each cycle 7.51663
+22.54988s current_cycle 3 | best_solution_cycle 0 | last_improvement_cycle 0
+Running the tail step
+43.06652s Cost of SP solution after tail step 486980.40153
+
+@Final_time 43.06886
+@Final_solution_cost 486980.40153
